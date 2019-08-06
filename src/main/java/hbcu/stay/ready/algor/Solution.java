114ba76796e20cc456dc57d7e6aa43d8514fa647
admin <admin@Briannas-MacBook-Pro.local>
2019-08-06 09:59:03 -0400

    done

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,23 @@
 package hbcu.stay.ready.algor;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Solution {
 
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+        Set<Boolean> answer = new LinkedHashSet<>();
+       String[] updatedMessage = message.split(" ");
+        String[] updatedMagazineText = magazineText.split(" ");
+        /*for(String a : updatedMagazineText){
+          if(answer.contains(updatedMessage)){
+              return true;
+          }
+
+        }*/
+
+
+        return updatedMagazineText.equals(updatedMessage);
     }
 }
